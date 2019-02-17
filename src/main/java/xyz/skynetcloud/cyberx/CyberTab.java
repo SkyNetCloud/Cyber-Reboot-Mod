a64986848d3f92c14941fd9e1c4e4ae466faddfc
SkyNetCloud <mcboxer1201@gmail.com>
2019-02-16 22:10:09 -0500

    Update Stuff
    Change The Icon to a Blocks

--- src/main/java/xyz/skynetcloud/cyberx/CyberTab.java
+++ src/main/java/xyz/skynetcloud/cyberx/CyberTab.java
@@ -1,21 +1,23 @@
 package xyz.skynetcloud.cyberx;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import xyz.skynetcloud.cyberx.init.BlockInit;
 import xyz.skynetcloud.cyberx.init.ItemInit;
 
 public class CyberTab extends CreativeTabs {
 
-
 	public CyberTab(String label) {
 		super(label);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public ItemStack getTabIconItem() {
-		return new ItemStack(ItemInit.DARK_STEEL);
+		return new ItemStack(BlockInit.VIBRANIUM_BLOCK);
 	}
 
 }

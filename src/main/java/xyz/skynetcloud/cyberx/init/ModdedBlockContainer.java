9ae9106262ee4a9c06f63e6118d7b1526677243c
SkyNetCloud <mcboxer1201@gmail.com>
2019-02-17 03:25:00 -0500

    Move Files
    I move this Files to Init

--- src/main/java/xyz/skynetcloud/cyberx/init/ModdedBlockContainer.java
+++ src/main/java/xyz/skynetcloud/cyberx/init/ModdedBlockContainer.java
@@ -1,19 +1,28 @@
-package xyz.skynetcloud.cyberx.blocks.container;
+package xyz.skynetcloud.cyberx.init;
 
 import net.minecraft.block.BlockContainer;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import xyz.skynetcloud.cyberx.Main;
-import xyz.skynetcloud.cyberx.init.BlockInit;
-import xyz.skynetcloud.cyberx.init.ItemInit;
 import xyz.skynetcloud.cyberx.util.interfaces.IHasModel;
 
 public class ModdedBlockContainer extends BlockContainer implements IHasModel {
 
+	public static final PropertyDirection FACING = BlockHorizontal.FACING;
+    protected static final AxisAlignedBB NORTH_CHEST_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0D, 0.9375D, 0.875D, 0.9375D);
+    protected static final AxisAlignedBB SOUTH_CHEST_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.875D, 1.0D);
+    protected static final AxisAlignedBB WEST_CHEST_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0625D, 0.9375D, 0.875D, 0.9375D);
+    protected static final AxisAlignedBB EAST_CHEST_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 1.0D, 0.875D, 0.9375D);
+    protected static final AxisAlignedBB NOT_CONNECTED_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.875D, 0.9375D);
+	
+	
 	public ModdedBlockContainer(String name, Material material, CreativeTabs tab) {
 		super(material);
 		setUnlocalizedName(name);
